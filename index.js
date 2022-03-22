803aa8c77d613e3589e98ef0272d2f53fca02d49
kyra qiu <kyraqiu@gmail.com>
2022-03-22 18:43:28 -0400

    commit tree 104f1e4378f6a0ffd16f6975f4b9144fd05c678d

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+const employee = {
+    //key:value
+    name:"Naruto Uzamaki",
+    streetAddress:"Konohagakure",
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {...employee,[key]:value,}
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key]=value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee,key){
+    const updatedEmp = delete {...employee[key]};
+    return {updatedEmp};
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee,key){   
+   delete employee[key];
+   return employee;
+    
+}
